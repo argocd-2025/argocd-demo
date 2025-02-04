b07744aa522acdc55c2d3e745f5f3b1988afabee
agunuworld4 <agunueghosa@gmail.com>
2025-02-03 22:47:58 -0600

    push

--- jjva-latest-version-update.sh
+++ jjva-latest-version-update.sh
@@ -2,6 +2,6 @@
 imageVersion="eagunuworld/jjva-mss-java-web-app:v$BUILD_NUMBER"
 ls -lart
 pwd
-cd argocd-demo/jjva-web-app
+cd /home/makutaworldmpm/jjva-GloudConnectworkingDirectory/workspace/argocd-project/argocd-jjva-web-app/jjva-web-app
 sed -i "s#cicd_latest_version#${imageVersion}#g" jjva-web-pod-manifest.yml
 cat jjva-web-pod-manifest.yml |grep  'eagunu'
