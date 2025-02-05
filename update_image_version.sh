091ccedb25bb7bcae9805b16e50bfe141b612992
agunuworld4 <agunueghosa@gmail.com>
2025-02-04 20:57:55 -0600

    push

--- update_image_version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-imageVersion="danle360/us-argocd-java-web-application:v$BUILD_NUMBER"
-sed -i "s#image_version_update#${imageVersion}#g" java_web_manifestFile.yml
-cat java_web_manifestFile.yml |grep  'danle'
